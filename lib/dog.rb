430836e76713bf104b994376fbe07fe74eba9d4a
Ryan M <rmccauleymusic@gmail.com>
2019-11-03 16:25:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,15 @@
 # Your code goes here!
 class Dog
-  :name => "Lassie"
+  def name=(this_dogs_name)
+    @this_dogs_name = dogs_name
+end
+
+def name
+  @this_dogs_name
+end
 end
 
 def bark
   puts "Woof"
 end
 
-dog.lassie(bark)
